dbe70372a5caa002890bcc2f6f73a0b31712407c
OldBigBuddha <shinlag@outlook.jp>
2018-12-18 12:24:50 +0200

    [fix] Source code
    Add npm scripts
    Add ignore for standard
    Fix sorce code with `standard --fix`

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -1,33 +1,31 @@
-const Router = require('express').Router();
-import { Feed } from 'feed';
-import trending from 'trending-github';
-import Moment from 'moment-timezone';
+import { Feed } from 'feed'
+import trending from 'trending-github'
+// import Moment from 'moment-timezone'
+const Router = require('express').Router()
 
-const updatedDate = new Date();
+const updatedDate = new Date()
 
-updatedDate.setHours(0);
-updatedDate.setMinutes(0);
-updatedDate.setSeconds(0);
-updatedDate.setMilliseconds(0);
-
+updatedDate.setHours(0)
+updatedDate.setMinutes(0)
+updatedDate.setSeconds(0)
+updatedDate.setMilliseconds(0)
 
 const feed = new Feed({
-  title: "GitHub Trend RSS",
-  description: "GitHub Trend",
-  id: "https://github.com/trending",
-  link: "https://github.com/trending",
-  generator: "https://github.com/mohikanz/github-trend-rss",
-  copyright: "(c) 2018 mohikanz",
+  title: 'GitHub Trend RSS',
+  description: 'GitHub Trend',
+  id: 'https://github.com/trending',
+  link: 'https://github.com/trending',
+  generator: 'https://github.com/mohikanz/github-trend-rss',
+  copyright: '(c) 2018 mohikanz',
   updated: updatedDate,
   feedLinks: {
-    atom: "https://rss.oldbigbuddha.net/feed"
+    atom: 'https://rss.oldbigbuddha.net/feed'
   }
-});
+})
 
-Router.get("/", (req, res) => {
-
-  const lang   = req.query.lang;
-  const period = req.query.period;
+Router.get('/', (req, res) => {
+  const lang = req.query.lang
+  const period = req.query.period
 
   trending(period, lang).then(repos => {
     repos.forEach(repo => {
@@ -37,17 +35,15 @@ Router.get("/", (req, res) => {
         link: repo.href,
         description: repo.description,
         date: updatedDate
-      });
-    });
+      })
+    })
 
-    feed.addCategory("Technology");
+    feed.addCategory('Technology')
 
-    console.log(updatedDate);
-    res.setHeader("Last-Modified", updatedDate);
-    res.status(200).send(feed.atom1());
+    console.log(updatedDate)
+    res.setHeader('Last-Modified', updatedDate)
+    res.status(200).send(feed.atom1())
+  })
+})
 
-  });
-
-});
-
-module.exports = Router;
+module.exports = Router
